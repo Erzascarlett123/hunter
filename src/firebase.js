609db4bb268308c6823e3057c1656e110b5376a8
erza <satriaseptian724@gmail.com>
2024-09-26 18:33:14 +0700

    terserah

--- src/firebase.js
+++ src/firebase.js
@@ -6,14 +6,14 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBPURS6MmefP_I5v_KUYexylG9r9RYVP70",
-  authDomain: "hunter-1232.firebaseapp.com",
-  databaseURL: "https://hunter-1232-default-rtdb.firebaseio.com",
-  projectId: "hunter-1232",
-  storageBucket: "hunter-1232.appspot.com",
-  messagingSenderId: "953233417537",
-  appId: "1:953233417537:web:9835c2f58be231e98f2dc0",
-  measurementId: "G-YFTXTESJYF"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
